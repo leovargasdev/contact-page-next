ccab04ee88b0764dd0dd43fcabe462d295ed5718
Leonardo Vargas <leu1607@hotmail.com>
2021-01-26 21:11:39 -0300

    Criando estrutura inicial

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    color: #FFF;
+    background: #0e0e0e;
+    /* font-family: 'Lumos'; */
+    -webkit-font-smoothing: antialiased;
+  }
+`
