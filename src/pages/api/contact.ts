721ff35cc6fb0f6ef08708a28ee8ff44efece8f1
Leonardo Vargas <leu1607@hotmail.com>
2021-01-30 10:45:14 -0300

    Testando env

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -3,27 +3,29 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const contact = async (request: NextApiRequest, response: NextApiResponse) => {
-  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
+  // const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
 
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_API_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_API_PRIVATE_KEY
-  })
-  await doc.loadInfo()
-  const sheet = doc.sheetsByIndex[0]
+  // await doc.useServiceAccountAuth({
+  //   client_email: process.env.GOOGLE_API_CLIENT_EMAIL,
+  //   private_key: process.env.GOOGLE_API_PRIVATE_KEY
+  // })
+  // await doc.loadInfo()
+  // const sheet = doc.sheetsByIndex[0]
 
-  await sheet.addRow({
-    name: 'Fulano',
-    email: 'fulano@hotmail.com',
-    phone: '5454545',
-    company: 'Sony',
-    message:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    date: new Date()
-  })
+  // await sheet.addRow({
+  //   name: 'Fulano',
+  //   email: 'fulano@hotmail.com',
+  //   phone: '5454545',
+  //   company: 'Sony',
+  //   message:
+  //     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  //   date: new Date()
+  // })
 
   return response.json({
-    funfou: 'ok'
+    funfou: 'ok',
+    key: process.env.GOOGLE_API_PRIVATE_KEY,
+    email: process.env.GOOGLE_API_CLIENT_EMAIL
   })
 }
 
