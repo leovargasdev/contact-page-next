c783dd9fa194414086ae25f901e6126d61c34a3b
Leonardo Vargas <leu1607@hotmail.com>
2021-01-30 10:31:58 -0300

    Adicionando método do google-sheet na api

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,36 +1,30 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import type { NextApiRequest, NextApiResponse } from 'next'
-// import { GoogleSpreadsheet } from 'google-spreadsheet'
-
-// type Data = {
-//   name: string
-// }
+import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const contact = async (request: NextApiRequest, response: NextApiResponse) => {
-  // const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
 
-  // await doc.useServiceAccountAuth({
-  //   client_email: process.env.GOOGLE_API_CLIENT_EMAIL,
-  //   private_key: process.env.GOOGLE_API_PRIVATE_KEY
-  // })
-  // await doc.loadInfo()
-  // const sheet = doc.sheetsByIndex[0]
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_API_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_API_PRIVATE_KEY
+  })
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0]
 
-  // await sheet.addRow({
-  //   name: 'Fulano',
-  //   email: 'fulano@hotmail.com',
-  //   phone: '5454545',
-  //   company: 'Sony',
-  //   message:
-  //     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-  //   date: new Date()
-  // })
+  await sheet.addRow({
+    name: 'Fulano',
+    email: 'fulano@hotmail.com',
+    phone: '5454545',
+    company: 'Sony',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    date: new Date()
+  })
 
   return response.json({
     funfou: 'ok'
   })
-  // response.status(200).json({
-  // })
 }
 
 export default contact
