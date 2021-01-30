a7d6173a1d0fb04d65aadafe136db995e6cb8a65
Leonardo Vargas <leu1607@hotmail.com>
2021-01-30 10:28:57 -0300

    Debugando api na vercel

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,37 +1,36 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { GoogleSpreadsheet } from 'google-spreadsheet'
+// import { GoogleSpreadsheet } from 'google-spreadsheet'
 
-type Data = {
-  name: string
-}
+// type Data = {
+//   name: string
+// }
 
-const contact = async (
-  request: NextApiRequest,
-  response: NextApiResponse<Data>
-) => {
-  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
+const contact = async (request: NextApiRequest, response: NextApiResponse) => {
+  // const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
 
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_API_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_API_PRIVATE_KEY
+  // await doc.useServiceAccountAuth({
+  //   client_email: process.env.GOOGLE_API_CLIENT_EMAIL,
+  //   private_key: process.env.GOOGLE_API_PRIVATE_KEY
+  // })
+  // await doc.loadInfo()
+  // const sheet = doc.sheetsByIndex[0]
+
+  // await sheet.addRow({
+  //   name: 'Fulano',
+  //   email: 'fulano@hotmail.com',
+  //   phone: '5454545',
+  //   company: 'Sony',
+  //   message:
+  //     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  //   date: new Date()
+  // })
+
+  return response.json({
+    funfou: 'ok'
   })
-  await doc.loadInfo()
-  const sheet = doc.sheetsByIndex[0]
-
-  await sheet.addRow({
-    name: 'Fulano',
-    email: 'fulano@hotmail.com',
-    phone: '5454545',
-    company: 'Sony',
-    message:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    date: new Date()
-  })
-
-  response.status(200).json({
-    name: 'ok'
-  })
+  // response.status(200).json({
+  // })
 }
 
 export default contact
